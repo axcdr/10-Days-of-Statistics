34aade5db98d3e98f2fca780ce509ce22af25dbc
axcdr <axthecdr@gmail.com>
2020-09-22 09:02:43 +0530

    Day 0

---
+++ Day 00_Weighted_Mean.py
@@ -0,0 +1,18 @@
+n = input()
+
+elements = raw_input()
+weights = raw_input()
+
+elements = elements.split(' ')
+weights = weights.split(' ')
+
+numerator = 0
+denominator = 0
+
+for i in range(0, len(elements)):
+    numerator = numerator + int(weights[i]) * int(elements[i])
+    denominator = denominator + int(weights[i])
+
+
+weighted_mean = numerator / float(denominator)
+print round(weighted_mean, 1)
