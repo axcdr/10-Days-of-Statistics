4989e58ea3177693bb31f38b4c91ef4d9bffb50e
axcdr <axthecdr@gmail.com>
2020-09-23 11:27:54 +0530

    Day 1 stats

---
+++ Day 01_Standard_Deviation.py
@@ -0,0 +1,16 @@
+import math
+
+n = float(raw_input())
+X = raw_input()
+
+X = X.split(' ')
+X = [int(x) for x in X]
+
+mean = sum(X) / n
+
+num = 0
+for x in X:
+    num = num + (x - mean)**2
+    
+std_dev = math.sqrt(num / n)
+print std_dev
